841cdf1ff85e3f0021b49c90b27d8085cd596b8e
hananta23 <hanantaadhi@gmail.com>
2021-11-18 21:49:31 +0700

    First Update

---
+++ Net5.JWT/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Net5.JWT.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net5.JWT.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "hananta", Password = "hananta", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "adhi", Password = "adhi", Role = "employee" });
+            users.Add(new User { Id = 3, Username = "rahmansyach", Password = "rahmansyach", Role = "tester" });
+            users.Add(new User { Id = 4, Username = "Admin", Password = "admin", Role = "Administrator" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
